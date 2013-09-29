1818780f36ae5fbdd7ae02b7a9d7166241762599
percy_g2 <gahlotpercy@gmail.com>
2013-09-29 16:14:41 +0530

    initial push

---
+++ cm.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/sony/nypon/full_nypon.mk)
+
+# Inherit CM common GSM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := cm_nypon
+PRODUCT_DEVICE := nypon
