de6f8f4a16e33172a7a53c0209d6bb143ccabddc
Rebel <gahlotpercy@gmail.com>
2013-10-19 19:22:04 +0530

    Update custom_nypon.mk

--- custom_nypon.mk
+++ custom_nypon.mk
@@ -24,7 +24,7 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/custom/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/sony/nypon/full_nypon.mk)
