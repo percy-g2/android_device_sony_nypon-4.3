401868ee3ec8fd73a2d75c84d63d882e40f6c8ee
Rebel <gahlotpercy@gmail.com>
2013-10-26 04:15:24 +0530

    Update omni_nypon.mk

--- omni_nypon.mk
+++ omni_nypon.mk
@@ -20,8 +20,8 @@
 # Inherit GSM configuration 
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
