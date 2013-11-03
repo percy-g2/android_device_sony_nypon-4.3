c82f449d18bf4da72c915cc61d1033072619f1d0
Rebel <gahlotpercy@gmail.com>
2013-11-04 05:03:43 +0530

    Update full_nypon.mk

--- full_nypon.mk
+++ full_nypon.mk
@@ -23,6 +23,13 @@
 #
 
 
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
